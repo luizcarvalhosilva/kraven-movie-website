82d6fff305fd8f16f08975c9728c9d6fbff0284f
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-01-09 18:16:04 -0300

    feat: implementar botão "de volta ao topo" e melhorias no JavaScript
    - Implementado botão "de volta ao topo" (HTML, CSS e JavaScript).
    - Melhorias gerais no JavaScript do projeto para maior eficiência e clareza.
    - Corrigido problema do menu que não fechava ao redimensionar a tela.
    - Removidos comentários desnecessários do código.

--- scripts/base-script.js
+++ scripts/base-script.js
@@ -1,11 +1,17 @@
-function displayMenu() {
-    const mobileMenu = document.querySelector(".mobile-menu");
-    const menuIcon = document.querySelector("#menu-icon");
-    if (mobileMenu.classList.contains("modal-open")) {
-        mobileMenu.classList.toggle("modal-open");
-        menuIcon.classList.toggle("bx-x");
-    } else {
-        mobileMenu.classList.toggle("modal-open");
-        menuIcon.classList.toggle("bx-x");
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuIcon = document.querySelector("#menu-icon");
+
+function displayMenu() {    
+    mobileMenu.classList.toggle("modal-open");
+    menuIcon.classList.toggle("bx-x");
+}
+
+function handleWidthChange(e) {
+    if(!e.matches) {
+        mobileMenu.classList.remove("modal-open");
+        menuIcon.classList.remove("bx-x");
     }
 }
+
+mediaQuery.addEventListener('change', handleWidthChange);
